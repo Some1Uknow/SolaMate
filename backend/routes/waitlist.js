644e42c17d5de3c616456b9610cb8a40fc5ad030
Some1Uknow <raghu250407@gmail.com>
2025-05-16 00:16:37 +0530

    migrating to nextjs

--- backend/routes/waitlist.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { joinWaitlist } from '../controllers/waitlistController.js';
-
-const router = express.Router();
-
-router.post('/join', joinWaitlist);
-
-export default router;
